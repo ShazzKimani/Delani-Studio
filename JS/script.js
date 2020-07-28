889c423c71d65279d3cc4ce25cce0d4c8bee871b
Sharonkim <57044724+ShazzKimani@users.noreply.github.com>
2020-07-28 12:19:24 +0300

    Added a function on the button onclick to alert the user we have recieved their message

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,9 @@ $('.main').click(function() {
   $(this).children('.hide').slideToggle(1200);
   $(this).children('.show').toggle(1500);
 });
+
+$("button").click(function (kami) {
+  var client = document.getElementById('client').value;
+  alert( client +' we have received your message' +'Thank you');
+  kami.preventDefault();
+});
